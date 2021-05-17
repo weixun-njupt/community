04d4cdf4dbf8437585ea0963cfdd5b318309c13e
weixun <wx18013966651@163.com>
2021-05-17 15:16:48 +0800

    init repo

---
+++ src/main/java/com/example/community/CommunityApplication.java
@@ -0,0 +1,16 @@
+package com.example.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+@SpringBootApplication
+public class CommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityApplication.class, args); }
+
+}
